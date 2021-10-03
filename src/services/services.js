047ad32e21ddc59ebd330077d0d81b4f264f89b8
BillelBenoudjit <hb_benoudjit@esi.dz>
2021-10-03 10:05:25 +0100

    Add endpoints for DataTable

--- src/services/services.js
+++ src/services/services.js
@@ -5,12 +5,20 @@ export const getHourlyEvents = async () => {
     return await axios.get(`${URL_ROOT}/events/hourly`)
 }
 
+export const getHourlyEventsWithPoi = async () => {
+    return await axios.get(`${URL_ROOT}/events/hourly/poi`)
+}
+
 export const getDailyEvents = async () => {
     return await axios.get(`${URL_ROOT}/events/daily`)
 }
 
 export const getHourlyStats = async () => {
     return await axios.get(`${URL_ROOT}/stats/hourly`)
+}
+
+export const getHourlyStatsWithPoi = async () => {
+    return await axios.get(`${URL_ROOT}/stats/hourly/poi`)
 }
 
 export const getDailyStats = async () => {
